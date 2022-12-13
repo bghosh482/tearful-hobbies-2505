45e93273fb4feff01cf14e1c4140c2445696ae1a
Bubai Ghosh <bghosh482@gmail.com>
2022-12-13 19:37:36 +0530

    update

---
+++ PayKaro/src/test/java/com/paykaro/PayKaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paykaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayKaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
