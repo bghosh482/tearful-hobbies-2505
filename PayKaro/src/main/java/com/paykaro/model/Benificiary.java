a8306bddb7f75dfc80804bb914bcab81f9ceefb1
Roshni gupta <roshni05011995@gmail.com>
2022-12-14 21:22:16 +0530

    --

--- PayKaro/src/main/java/com/paykaro/model/Benificiary.java
+++ PayKaro/src/main/java/com/paykaro/model/Benificiary.java
@@ -11,5 +11,26 @@ public class Benificiary {
 	private Integer bid;
 	private String name;
 	private String mobileNo;
+	
+	public Integer getBid() {
+		return bid;
+	}
+	public void setBid(Integer bid) {
+		this.bid = bid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	
 
 }
