dbbf0bb4629ac2dc6fe27cd1628ea48cc8017dd3
Bubai Ghosh <bghosh482@gmail.com>
2022-12-16 22:25:04 +0530

    added

--- PayKaro/src/main/java/com/paykaro/controller/CustomerController.java
+++
@@ -1,8 +0,0 @@
-package com.paykaro.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CustomerController {
-
-}
--- PayKaro/src/main/java/com/paykaro/repository/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.paykaro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.paykaro.model.Customer;
-
-public interface CustomerDAO extends JpaRepository<Customer, Integer> {
-
-}
