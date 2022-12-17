b9635dc5aeee0f81334bf613bd89a57825381693
Bubai Ghosh <bghosh482@gmail.com>
2022-12-17 21:17:49 +0530

    added

--- PayKaro/src/main/java/com/paykaro/exception/AccountException.java
+++
@@ -1,5 +0,0 @@
-package com.paykaro.exception;
-
-public class AccountException {
-
-}
--- PayKaro/src/main/java/com/paykaro/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.paykaro.service;
-
-import com.paykaro.exception.CustomerException;
-import com.paykaro.exception.WalletException;
-import com.paykaro.model.Account;
-
-public interface AccountService {
-	public Account addAccount(Account account, String key) throws CustomerException, WalletException;
-}
